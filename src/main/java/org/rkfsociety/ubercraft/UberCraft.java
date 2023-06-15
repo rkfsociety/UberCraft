b9d721a505370ea17c2be0ddd029de1d02a3039d
rkfsociety <romankuzminvital@gmail.com>
2023-06-16 00:12:52 +0200

    1.0

---
+++ src/main/java/org/rkfsociety/ubercraft/UberCraft.java
@@ -0,0 +1,17 @@
+package org.rkfsociety.ubercraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UberCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
